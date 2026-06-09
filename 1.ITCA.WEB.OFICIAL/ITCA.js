83ecf05993832bbbbb36d806dcc1be3ba478de40
Stiveen <infosteveen@gmail.com>
2026-06-09 17:44:43 -0600

    pantera

--- 1.ITCA.WEB.OFICIAL/ITCA.js
+++ 1.ITCA.WEB.OFICIAL/ITCA.js
@@ -502,15 +502,6 @@ galeria: `
     </section>
     `,
 
-document.getElementById("topBtn")
-.addEventListener("click", () => {
-
-    window.scrollTo({
-        top:0,
-        behavior:"smooth"
-    });
-
-});
     
 
 function cargarSeccion(nombre) {
